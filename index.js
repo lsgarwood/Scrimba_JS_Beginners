8915be50cbaf7e645134e38b88ce2c93d7bdf51a
Lauren <ls_garwood@yahoo.co.uk>
2022-04-16 19:20:04 +0100

    LG: creating save feature

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
 function increment() {
-    count = count + 1;
+    count += 1; 
     countEl.innerText = count;
+}
+
+function save() {
+    let entries = " " + count + " - ";
+    saveEl.innerText += entries;
 }
